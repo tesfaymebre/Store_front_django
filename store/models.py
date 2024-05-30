ada64b2496a3267ce9f0fb360443734cb4995ec4
Tesfamichael Asfaw <tesfaymebre@gmail.com>
2024-05-30 13:24:18 +0300

    update customers class

--- store/models.py
+++ store/models.py
@@ -29,18 +29,12 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold')
     ]
-    first_name = models.CharField(max_length=255)
+    given_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-
-    class Meta:
-        db_table = 'store_cutomers'
-        indexes = [
-            models.Index(fields=['last_name', 'first_name'])
-        ]
 
 class Order(models.Model):
     PENDING = 'P'
